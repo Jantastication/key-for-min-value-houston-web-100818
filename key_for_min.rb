107c79aae293b46a55dd37e85b9d3e6f95856b1c
Jantastication O <georginaoregx@gmail.com>
2018-10-06 05:35:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
        nil
    else
-
+     min_value =float::INFINITY
      name_hash.each do |key, value|
      if value < min_value
      min_value = value
